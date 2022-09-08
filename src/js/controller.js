83bf690134372bb3ba5dc4f15cc40ff948cc0ee3
TeoKiz <teo_kiz@hotmail.com>
2022-09-08 20:12:04 +0300

    Cleaned up Controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,10 +11,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -51,7 +47,6 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3. render results
-    // resultsView.render(model.state.search.results);
     resultsView.render(model.getSearchResultsPage());
 
     // 4. Render initial pagination buttons
