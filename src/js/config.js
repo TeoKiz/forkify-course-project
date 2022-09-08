ec3f8126f8f109f051fe23818c28dd43768ac419
TeoKiz <teo_kiz@hotmail.com>
2022-09-08 18:45:00 +0300

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '146307fd-e573-45df-9b1e-514f427db1e5';
+export const MODAL_CLOSE_SEC = 2.5;
